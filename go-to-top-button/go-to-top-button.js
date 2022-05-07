507de0c2d717d1d4ebe8df1d0ccdcf0c92ba8ebf
Vacalexis <alex_til@hotmail.com>
2022-05-07 18:24:55 +0100

    Added header with avatar and name. Extracted code into separate files. Added more content.

---
+++ go-to-top-button/go-to-top-button.js
@@ -0,0 +1,18 @@
+let goToTopButton = document.getElementById('goToTopButton');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goToTopButton.style.display = "block";
+  } else {
+    goToTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
